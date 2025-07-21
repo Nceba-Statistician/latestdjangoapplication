c6f96de8d310277ea92285ecaeb64062a8c393f0
Nceba-Developer <143310721+Nceba-Developer@users.noreply.github.com>
2025-07-21 16:25:37 +0200

    updated

--- app/urls.py
+++ app/urls.py
@@ -5,7 +5,6 @@ app_name = "Application"
 
 urlpatterns = [
     path("page", views.frontpage, name="page"),
-    # path("", views.sidebar, "sidebars"),
     path("", views.product_list_view, name='product_list'),
     path('<int:pk>/', views.product_detail_view, name='product_detail'),
 ]
