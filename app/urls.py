eb5dd6eba5ce31e4ec16029def4e0b27102f9707
Nceba-Developer <143310721+Nceba-Developer@users.noreply.github.com>
2025-07-21 15:23:08 +0200

    updated

--- app/urls.py
+++ app/urls.py
@@ -5,7 +5,7 @@ app_name = "Application"
 
 urlpatterns = [
     path("page", views.frontpage, name="page"),
-    path("", views.sidebar, "sidebar"),
+    # path("", views.sidebar, "sidebars"),
     path("", views.product_list_view, name='product_list'),
     path('<int:pk>/', views.product_detail_view, name='product_detail'),
 ]
--- app/views.py
+++ app/views.py
@@ -4,23 +4,23 @@ from django.http import HttpResponse
 def frontpage(request):
     return HttpResponse('<p2>Welcome to our awesome application!</p2>') 
 
-def sidebar(request):
-    return render(request, "components/base.html")
+# def base(request):
+    # return render(request, "components/base.html")
 
 def home_view(request):
-    return render(request, 'home.html', {'page_title': 'Home'})
+    return render(request, 'components/home.html', {'page_title': 'Home'})
 
 def about_view(request):
-    return render(request, 'about.html', {'page_title': 'About Us'})
+    return render(request, 'components/about.html', {'page_title': 'About Us'})
 
 def product_list_view(request):
     products = [
         {'id': 1, 'name': 'Product A'},
         {'id': 2, 'name': 'Product B'},
     ]
-    return render(request, 'products/product_list.html', {'page_title': 'Products', 'products': products})
+    return render(request, 'components/products/product_list.html', {'page_title': 'Products', 'products': products})
 
 def product_detail_view(request, pk):
     # In a real app, you'd fetch from a database
     product = {'id': pk, 'name': f'Product {chr(64 + pk)}'}
-    return render(request, 'products/product_detail.html', {'page_title': f'Product {pk}', 'product': product})
+    return render(request, 'components/products/product_detail.html', {'page_title': f'Product {pk}', 'product': product})
